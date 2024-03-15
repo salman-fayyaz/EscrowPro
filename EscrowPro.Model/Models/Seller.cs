ba9d8d8be8e86ad3a93147b7cd604c9b2cbb699e
salman-fayyaz <sfayyaz8c@gmail.com>
2024-03-15 11:05:16 +0500

    Add project files.

---
+++ EscrowPro.Model/Models/Seller.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscrowPro.Model.Models
+{
+    public class Seller
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]   
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public int CNIC { get; set; }
+
+        public int Phone { get; set; }
+
+        public DateTime RegistrationDate { get; set; }
+
+        public  ICollection<Transaction> Transactions { get; set; }
+    }
+}
---
+++ EscrowPro.Model/Models/Transaction.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscrowPro.Model.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+
+        public int Amount { get; set; }
+
+        public DateTime StartDate {  get; set; }
+
+        public DateTime CompletionDate { get; set; }
+
+        public int BuyerId { get; set; }
+
+        public Buyer Buyer { get; set; }
+
+        public int SellerId {  get; set; }
+
+        public Seller Seller { get; set; }
+    }
+}
