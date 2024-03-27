0130c858f2467be69db7fef036c677fff18ed267
salman-fayyaz <sfayyaz8c@gmail.com>
2024-03-27 12:46:37 +0500

    making dbContext

---
+++ EscrowPro.Infrastructure/Data/EscrowProContext.cs
@@ -0,0 +1,26 @@
+﻿using EscrowPro.Model.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace EscrowPro.Infrastructure.Data
+{
+    public class EscrowProContext : DbContext
+    {
+        public DbSet<Buyer>Buyers { get; set; }
+
+        public DbSet<Seller> Sellers { get; set; }
+
+        public DbSet<Transaction> Transactions { get; set; }
+
+        public DbSet<Payment> Payments { get; set; }
+
+        public DbSet<Status> Statuses { get; set; }
+
+        public DbSet<Product> Products {  get; set; }
+
+        public DbSet<Escrow> Escrows { get; set; }
+
+        public DbSet<Dispute> Disputes { get; set; }    
+
+    }
+}
